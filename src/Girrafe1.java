86f2c5048e7a7eebb0ae21278484ed4954fb774f
Anderson Kongoda <kongoda11@gmail.com>
2021-05-27 15:37:00 -0400

    Add age4 and age2/age1

---
+++ src/Girrafe1.java
@@ -0,0 +1,42 @@
+
+public class Girrafe1 {
+
+	/*
+	 * Addition  		==> +
+	 * Subtraction 		==> -
+	 * Multiplication   ==> *
+	 * Division 		==> /
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		int age1, age2, age3, age4;
+		
+		int sum, difference;
+		int multiply, divide;
+		
+		age1=2;
+		age2=4;
+		age3=6;
+		age4=7;
+		
+		
+		sum = age1 + age2 + age3; // Addition
+		difference = age1-age2-age3; //Difference
+		divide = age2/age1;  //Divide
+		multiply = age1*age2*age3*age4; //multiplication
+		
+		
+		System.out.println("The total age of the three girrafes is " + sum);
+		System.out.println("The difference age of the three girrafes is " + difference);
+		System.out.println("Age1 divide by age2 " + divide);
+		System.out.println("age1*age2*age3 " + multiply);
+		
+		
+		
+		
+
+	}
+
+}
